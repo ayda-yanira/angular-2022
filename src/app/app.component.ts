08ef05e40112361baab3ff6f7b98b3f9e65d5be8
yanira <yanira.benavides@katary.co>
2022-07-25 15:15:16 -0500

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiPrimerApp';
+  title = 'HOLA';
+
+  public ArregloTarjetas: Tarjeta[]=[]
+  ngOnInit(): void {
+    this.ArregloTarjetas=[
+    {titulo:'video', subtitulo:'subtitulo video 1', nro:5},
+    {titulo:'video', subtitulo: 'subtitulo video 2', nro:6},
+    {titulo:'video', subtitulo: 'subtitulo video 3', nro:7},
+]
+}
 }
